92e5d52f31993f57d91e253dd8dc0fcd7205355a
czc222 <1263291450@qq.com>
2021-11-26 13:10:26 +0800

    引入svg

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import * as React from 'react';
+require('icons/money.svg') //使用require防止 treeShaking
+require('icons/labels.svg')
+require('icons/statistics.svg')
+
+
+
 
 const NavWrapper=styled.div`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,7 +16,15 @@ const NavWrapper=styled.div`
     li{
       width:33.33%;
       text-align:center;
-      padding:16px;
+      padding:4px 0;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon{
+        width: 24px;
+        height:24px;
+      }
     }
   }
 `
@@ -20,12 +34,21 @@ const Nav=()=>{
       <ul>
 
         <li>
+          <svg className="icon">
+            <use xlinkHref="#labels" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
 
