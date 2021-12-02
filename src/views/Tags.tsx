ee6004915252721798221e4af1aeb81ec2cea747
czc222 <1263291450@qq.com>
2021-12-02 20:29:19 +0800

    tag List样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,6 +1,37 @@
 import LayoutWrapper from '../components/LayoutWrapper';
 import * as React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+
+
+const TagList = styled.ol`
+  font-size: 16px; 
+  background:white;
+  > li{
+    //#e5e5e7
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px; border: none; padding: 8px 12px;
+  background: #80b3ff; border-radius: 4px;
+  color: white;
+`;
+const Center = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`
 
 function Tags() {
   const {tags,setTags} =useTags()
@@ -8,11 +39,20 @@ function Tags() {
 
     <LayoutWrapper>
       <ol>
-        {tags.map(tag=>
-          <li key={tag}>
-            {tag}
-          </li>)}
+        <TagList>
+          {tags.map(tag=>
+            <li key={tag}>
+              <span className="oneLine">{tag}</span>
+            </li>)}
+        </TagList>
+
       </ol>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </Center>
 
     </LayoutWrapper>
 
