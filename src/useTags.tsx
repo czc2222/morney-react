eaf89bd4cff64fc04353dae3761c9467e3d59033
czc222 <1263291450@qq.com>
2021-12-03 15:06:37 +0800

    修复bug

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,16 +11,16 @@ const defaultTags=[
 const useTags=()=>{
   const [tags, setTags] = React.useState<{id:number,name:string}[]>(defaultTags)
   const findTag=(id:number)=> tags.filter(tag=>tag.id===id)[0]
-  const findTagIndex=(id:number)=>{tags.forEach(tag=>tag.id === id)}
-  // const findTagIndex=(id:number)=>{
-  //   let result =-1
-  //   for(let i=0;i<tags.length;i++){
-  //     if(tags[i].id === id){
-  //       result = i
-  //       break
-  //     }
-  //   }
-  // }
+  const findTagIndex=(id:number)=>{
+    let result =-1
+    for(let i=0;i<tags.length;i++){
+      if(tags[i].id === id){
+        result = i
+        break
+      }
+    }
+      return result
+  }
 
   const updateTag=(id:number,name:string)=>{
     const tagClone =JSON.parse(JSON.stringify(tags)) //数据进行深拷贝 才能改写
